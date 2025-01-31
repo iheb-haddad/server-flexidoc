76fe468bd6d1f84e50900c55762332d04d7e06d5
iheb-haddad <hadediheb9@gmail.com>
2025-01-31 21:47:57 +0100

    updates

--- models/section.model.js
+++ models/section.model.js
@@ -4,17 +4,20 @@ const Schema = mongoose.Schema;
 const sectionSchema = new Schema(
   {
     titleFr: { type: String },
-    titleEn: { type: String },
-    customTitleFr: { type: String },
-    customTitleEn: { type: String },
+    customTitles: [
+      {
+        title: { type: String },
+        language: { type: String }
+      }
+    ],
     titlePolice: { type: String },
     textPolice: { type: String },
     titleColor: { type: String },
     textColor: { type: String },
     backgroundColor: { type: String },
-    fontSizeTitle : { type: String },
-    fontSizeText : { type: String },
-    paddingUnderTitle : { type: String },
+    fontSizeTitle: { type: String },
+    fontSizeText: { type: String },
+    paddingUnderTitle: { type: String },
     traitDisplay: { type: String },
     traitColor: { type: String },
     traitWidth: { type: String },
