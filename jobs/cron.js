7305662ed71dbdb66cdf8e4b028129df3811c5bf
iheb-haddad <hadediheb9@gmail.com>
2025-04-25 12:18:01 +0200

    updates cron job

--- jobs/cron.js
+++ jobs/cron.js
@@ -2,14 +2,18 @@ const cronJobs = require("node-cron");
 const moment = require("moment");
 const Documentation = require("../models/documentation.model");
 
-cronJobs.schedule("*/10 * * * *", async () => {
+cronJobs.schedule("*/1 * * * *", async () => {
   try {
     const documents = await Documentation.find();
     for (const document of documents) {
-      if (moment(document.expiration).isBefore(moment(), "day")) {
+      if (moment(document.expiration).isBefore(moment(), "day") && document.status !== "expiré") {
         await Documentation.updateOne(
           { _id: document._id },
-          { statut: "expiré" }
+          { status: "expiré" }
+        );
+
+        console.log(
+          `Document with ID ${document.title} has been updated to expired.`
         );
       }
     }
